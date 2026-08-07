167275c351390b2c59481f76751a4bf86229a067
HudaMuhammed <hoda157545@gmail.com>
2026-08-07 13:15:19 +0300

    The tablet layout is done

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/widgets/dashboard_tablet_layout.dart';
 
 import '../widgets/dashboard_desktop_layout.dart';
 
@@ -12,7 +13,7 @@ class DashboardView extends StatelessWidget {
       backgroundColor: const Color(0xFFF7F9FA),
         body: AdaptiveLayoutWidget(
       mobileLayout: (context) => const SizedBox(),
-      tabletLayout: (context) => const SizedBox(),
+      tabletLayout: (context) => const DashboardTabletLayout(),
       desktopLayout: (context) => const DashboardDesktopLayout(),
     ));
   }
--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/widgets/custom_dot.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/dots_indicator.dart';
 import 'package:responsive_dash_board/widgets/income_section.dart';
-import 'package:responsive_dash_board/widgets/latest_transaction_list_view.dart';
 import 'package:responsive_dash_board/widgets/my_card_and_transaction_history_section.dart';
-import 'package:responsive_dash_board/widgets/my_cards_section.dart';
-import 'package:responsive_dash_board/widgets/transaction_history.dart';
 import 'all_expensess_and_quick_invoice_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -22,11 +17,24 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(
           width: 32,
         ),
-        Expanded(flex: 2, child: AllExpensessAndQuickInvoiceSection(),),
+        Expanded(
+          flex: 2,
+          child: AllExpensessAndQuickInvoiceSection(),
+        ),
         SizedBox(
           width: 24,
         ),
-        Expanded(child: IncomeSection()),
+        Expanded(
+            child: Column(
+          children: [
+            SizedBox(height: 40,),
+            MyCardAndTransactionHistorySection(),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(child: IncomeSection()),
+          ],
+        )),
       ],
     );
   }
